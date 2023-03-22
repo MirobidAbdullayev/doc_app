bfad79cb706f5741e0eaa0dc0fad31f942f77ece
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-03-22 17:05:40 +0500

    login validate

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointments;
+use App\Models\User;
+use App\Models\Doctor;
+use App\Models\UserDetails;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * login
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $reqeust)
+    {
+        //validate incoming inputs
+        $reqeust->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        //check matching user
+        $user = User::where('email', $reqeust->email)->first();
+
+        //check password
+        if(!$user || ! Hash::check($reqeust->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email'=>['The provided credentials are incorrect'],
+            ]);
+        }
+
+        //then return generated token
+        return $user->createToken($reqeust->email)->plainTextToken;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
