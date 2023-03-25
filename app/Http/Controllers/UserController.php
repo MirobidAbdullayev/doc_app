5bd34a2bf5c8f044b8fee495122cca40398df8db
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-03-25 18:02:50 +0500

    ....

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,22 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $user = array();
+        $user = Auth::user();
+        $doctor = User::where('type', 'doctor')->get();
+        $doctorData = Doctor::all();
+
+        foreach($doctorData as $data){
+            foreach($doctor as $info){
+                if($data['doc_id'] == $info['id']){
+                    $data['doctor_name'] = $info['name'];
+                    $data['doctor_profile'] = $info['profile_photo_url'];
+                }
+            }
+        }
+
+        $user['doctor'] = $doctorData;
+        return $user; 
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::post('/login', [UserController::class, 'login']);
 
 Route::post('/register', [UserController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', [UserController::class, 'index']);
 });
